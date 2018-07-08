d654a61b0ec194a6fa73f43fbc19917a6b714e4a
Dorian Karter <dkarter@users.noreply.github.com>
2018-07-08 02:49:34 -0500

    Add network configuration

--- config/config.exs
+++ config/config.exs
@@ -12,6 +12,18 @@ config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 # Use shoehorn to start the main application. See the shoehorn
 # docs for separating out critical OTP applications such as those
 # involved with firmware updates.
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
+config :nerves_network, :default,
+  wlan0: [
+    ssid: System.get_env("NERVES_NETWORK_SSID"),
+    psk: System.get_env("NERVES_NETWORK_PSK"),
+    key_mgmt: String.to_atom(key_mgmt)
+  ],
+  eth0: [
+    ipv4_address_method: :dhcp
+  ]
+
 config :shoehorn,
   init: [:nerves_runtime, :nerves_init_gadget],
   app: Mix.Project.config()[:app]
